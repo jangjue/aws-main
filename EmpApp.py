9e0b77919f09df2110bb3038d79c9c4bc8769c6c
jangjue <jangjue98@gmail.com>
2022-10-05 17:12:54 +0800

    fix search

--- EmpApp.py
+++ EmpApp.py
@@ -92,7 +92,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return redirect('/')
+    return redirect('/', name=emp_name)
 
 
 @app.route("/searchemp",methods=['POST','GET'])
